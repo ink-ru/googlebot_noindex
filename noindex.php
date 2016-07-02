579254c8e9f50cfab8d3b80d748c26937ea76eaa
Васин Юрий <george.a.wise@gmail.com>
2016-07-02 08:52:46 +0300

    Update noindex.php

--- noindex.php
+++ noindex.php
@@ -19,7 +19,6 @@ function GoogleBotDirective(&$content)
     $noindex = '<meta name="googlebot" content="noindex">';
     $buffered = false;
     
-    
     if(@file_exists($path)) $sR = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         else $sR = file('http://'.$_SERVER['SERVER_NAME'].'/robots.txt');
     $sR = array_unique(array_filter($sR));
@@ -27,38 +26,24 @@ function GoogleBotDirective(&$content)
     foreach($sR as $sRule) 
     {
         if(strpos($sRule, '#')) $sRule = preg_replace('~^([^#]*)#~i', "$1", $sRule); // comment removing
-        
         if(!preg_match('~Disallow.*~', $sRule)) continue; // нам нужны только запрещающие правила
         
-        if($content !== false)
+        if($content !== false && strlen($content) > 100)
         {
           if(!preg_match('~<meta[^>]+name\s*=\s*["|\']googlebot["|\'][^>]+noindex[^>]+>~siU', $content)) $buffered = true;
             else continue;
         }
 
-        $sRepFrom = array( '*',  '?' );
-        $sRepTo =   array( '.*', '\?');
-
         $sRule = preg_replace('~^\s*Disallow\s*:\s*~i', '', $sRule);
-        $sRule = trim(str_replace($sRepFrom,$sRepTo,$sRule));
-
-        // Жадность
-        // if(!strpos($sRule,'$')) $sRule = preg_replace('~([^\*])$~i', "$1.*", $sRule);
-        }
-        
+        $sRule = trim(str_replace(array( '*',  '?' ), array( '.*', '\?'), $sRule));
         
         if(preg_match('~'.$sRule.'~i', $_SERVER['REQUEST_URI']))
         {
             if($buffered) $content = str_ireplace('</title>', '</title>'.$noindex, $content);
               else print $noindex;
-            
             return true;
-            break;
         }
-        
     }
-    
     return false;
-    
 }
 ?>
